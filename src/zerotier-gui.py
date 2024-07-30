2c18bbc9e33afb57a8a164959d213fcfa4e31308
Aaron Collins <aaron777collins@gmail.com>
2024-07-30 01:04:37 -0400

    Release version 2.2.3

--- src/zerotier-gui.py
+++ src/zerotier-gui.py
@@ -707,7 +707,7 @@ class MainWindow:
         ztGuiVersionLabel = tk.Label(
             middleFrame,
             font="Monospace",
-            text="{:40s}{}".format("ZeroTier GUI (Upgraded) Version:", "2.2.2"),
+            text="{:40s}{}".format("ZeroTier GUI (Upgraded) Version:", "2.2.3"),
             bg=BACKGROUND,
             fg=FOREGROUND,
         )
@@ -1367,7 +1367,7 @@ def run_zerotier_cli(*args, stderr_to_stdout=False):
     return stdout.decode()
 
 def ask_sudo_password():
-    def save_password():
+    def save_password(event=None):
         nonlocal password
         password = passwordEntry.get()
         passwordWindow.destroy()
