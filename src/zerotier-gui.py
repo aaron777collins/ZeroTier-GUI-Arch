d4d22b8eab2dbe2b688cd8369c96f4b739ac053d
Aaron Collins <aaron777collins@gmail.com>
2024-07-29 22:14:56 -0400

    Release version 2.1.5

--- src/zerotier-gui.py
+++ src/zerotier-gui.py
@@ -707,7 +707,7 @@ class MainWindow:
         ztGuiVersionLabel = tk.Label(
             middleFrame,
             font="Monospace",
-            text="{:40s}{}".format("ZeroTier GUI (Upgraded) Version:", "2.1.1"),
+            text="{:40s}{}".format("ZeroTier GUI (Upgraded) Version:", "2.1.5"),
             bg=BACKGROUND,
             fg=FOREGROUND,
         )
